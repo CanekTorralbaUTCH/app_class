2d6223bd3d2a3ca42d7e63c2433edea34f2ed8f2
CanekTorralba <a6519150036@utch.edu.mx>
2021-08-07 21:04:55 -0600

    Added BadgesScreen.js

---
+++ src/components/BadgesScreen/BadgesScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native'
+import BadgesItem from './BadgesItem';
+import Colors from '../../res/Colors'
+import Http from '../../libs/http'
+
+class BadgesScreen extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>BadgesScreen</Text>
+            </View>
+        );
+    }
+}
+
+export default BadgesScreen;
