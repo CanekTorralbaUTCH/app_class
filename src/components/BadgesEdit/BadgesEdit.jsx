1748892bc4949fb06ba6010e8be4bb4fb356389a
CanekTorralba <a6519150036@utch.edu.mx>
2021-08-08 13:44:01 -0600

    Create BadgesEdit.jsx

---
+++ src/components/BadgesEdit/BadgesEdit.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    ScrollView,
+    TouvhableOpacity,
+    Image,
+    ActivityIndicator,
+    TouchableOpacity,
+} from 'react-native';
+import Colors from '../../res/Colors'
+import Http from '../../libs/http'
+
+//Class that will allow us to edit the badges
+class BadgesEdit extends react.Component{
+    
+    //Render the screen
+    render(){
+        return(
+            <View>
+                <Text>Badges Edit</Text>
+            </View>
+        );
+    }
+}
+
+
+
+export default BadgesEdit;
