4676b2ea397ec4f2b3f306a67152d3b5d3b08841
CanekTorralba <a6519150036@utch.edu.mx>
2021-08-08 12:14:48 -0600

    Add base code of BadgesDetail.js

---
+++ src/components/BadgesDetail/BadgesDetail.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native'
+import Colors from '../../res/Colors'
+
+//Class whwre it is shown the details of the user's acount
+class BadgesDetail extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>Badges Detail</Text>
+            </View>
+        );
+    }
+}
+
+export default BadgesDetail;
