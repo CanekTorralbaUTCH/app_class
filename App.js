c2109c9252a1534091f0037eb25f8e1adfbc6f6f
CanekTorralba <a6519150036@utch.edu.mx>
2021-08-08 16:43:00 -0600

    Fatal error corrected

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import BadgesStack from './src/components/BadgesScreen/BadgesStack';
 
 const App = () => {
-  return <NavigationContainer />;
+  return <NavigationContainer>
+    {/* This will allow us to travel between screens, DO NOT FORGET TO ADD */}
+    <BadgesStack />
+  </NavigationContainer>
 };
 
 export default App;
