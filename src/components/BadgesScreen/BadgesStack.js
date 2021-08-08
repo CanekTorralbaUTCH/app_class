6fec8423afa846372a6f484bf673e02f1c3fcc67
CanekTorralba <a6519150036@utch.edu.mx>
2021-08-07 21:01:56 -0600

    Added to the screens BadgesScreen.js

--- src/components/BadgesScreen/BadgesStack.js
+++ src/components/BadgesScreen/BadgesStack.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator} from '@react-navigation/stack';
 import BadgeLanding from '../BadgesLanding/BadgesLanding';
 import Colors from '../../res/Colors';
+import BadgesScreen from './BadgesScreen';
 
 const Stack = createStackNavigator();
 
@@ -9,16 +10,19 @@ const Stack = createStackNavigator();
 const BadgesStack = () => {
     return(
         //The part where the characteristics of the main screen are defined
-        <Stack.Navigator screenOptions={{
-            //In this case the header
-            headerStyle: {
-                backgroundColor: Colors.blackPearl,
-                shadowColor: Colors.blackPearl,
-            },
-            headerTintColor: Colors.white,
+        <Stack.Navigator 
+            screenOptions={{
+                //In this case the header
+                headerStyle: {
+                    backgroundColor: Colors.blackPearl,
+                    shadowColor: Colors.blackPearl,
+                },
+                headerTintColor: Colors.white,
         }}>
-            //Here we have the badge of the Landing page
+            {/* Here we have the badge of the Landing page */}
             <Stack.Screen name="Landing" component={BadgeLanding} options={{headerShown: false}}/>
+            {/* Here we have the the screen for the user's badges */}
+            <Stack.Screen name="Badges" component={BadgesScreen}/>
         </Stack.Navigator>
     );
 };
