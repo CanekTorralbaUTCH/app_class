ff8f7257817e7005f35dc9badca2917b4228be70
CanekTorralba <a6519150036@utch.edu.mx>
2021-08-08 19:14:34 -0600

    Create url.js with urls to use

---
+++ src/libs/url.js
@@ -0,0 +1,7 @@
+//URLs where we can test the app
+let URLS = {
+    badges_url: 'https://api-example-badges-by-og.herokuapp.com',
+    users_url: 'https://djangoapiog.herokuapp.com/'
+}
+
+export default URLS
