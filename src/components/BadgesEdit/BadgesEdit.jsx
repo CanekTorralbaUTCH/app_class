10e94a255ed5b12e10ee5e5b41719e3cc1a762e7
CanekTorralba <a6519150036@utch.edu.mx>
2021-08-08 20:05:24 -0600

    Style sheet edition

--- src/components/BadgesEdit/BadgesEdit.jsx
+++ src/components/BadgesEdit/BadgesEdit.jsx
@@ -155,6 +155,7 @@ const styles = StyleSheet.create({
     content:{
         flex:1,
         margin: 20,
+        marginTop: 30,
         width:'90%',
         height: '90%',
         backgroundColor: Colors.white,
