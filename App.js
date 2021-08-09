92318875f838cec5a19e816f6f4b021eb995b48c
CanekTorralba <a6519150036@utch.edu.mx>
2021-08-08 19:29:36 -0600

    Navigation changed from BadgeStack to AppStack

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import BadgesStack from './src/components/BadgesScreen/BadgesStack';
+import AppStack from './src/components/AppNavigator/AppStack';
 
 const App = () => {
   return <NavigationContainer>
     {/* This will allow us to travel between screens, DO NOT FORGET TO ADD */}
-    <BadgesStack />
+    <AppStack />
   </NavigationContainer>
 };
 
