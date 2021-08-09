a1f44fd07f56455277aa0a1855cb3dc322e72fb7
CanekTorralba <a6519150036@utch.edu.mx>
2021-08-08 21:25:45 -0600

    Buttons edition

--- src/components/BadgesScreen/BadgesItem.js
+++ src/components/BadgesScreen/BadgesItem.js
@@ -39,14 +39,21 @@ class BadgesItem extends React.Component {
                 </TouchableOpacity>
                 {/* View for the edit and delete options */}
                 <View style={styles.icons}>
-                    <Pressable onPress={this.props.onEdit}>
-                        <Image style={styles.editIcon}
-                        source={require('../../assets/edit_icon.png')} />
-                    </Pressable>
-                    <Pressable onPress={this.props.onDelete}>
-                        <Image style={styles.deleteIcon}
-                        source={require('../../assets/delete_icon.png')} />
-                    </Pressable>
+                    {this.props.onEdit ? (
+                        <Pressable onPress={this.props.onEdit}>
+                            <Image
+                                style={styles.editIcon}
+                                source={require('../../assets/edit_icon.png')} />
+                        </Pressable>
+                    ) : null}
+                    {this.props.onDelete ? (
+                        <Pressable onPress={this.props.onDelete}>
+                            <Image
+                                style={styles.deleteIcon}
+                                source={require('../../assets/delete_icon.png')} />
+                        </Pressable>
+                    ) : null}
+                    
                 </View>
             </View>
         );
