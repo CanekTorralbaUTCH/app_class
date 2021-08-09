b5816be762b6ab3bd35468d32ea055535d7f0156
CanekTorralba <a6519150036@utch.edu.mx>
2021-08-08 22:46:41 -0600

    fix bug

--- src/components/BadgesDetail/BadgesDetail.js
+++ src/components/BadgesDetail/BadgesDetail.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native'
 import Colors from '../../res/Colors'
+import Storage from "../../libs/storage"
 
 //Class whwre it is shown the details of the user's acount
 class BadgesDetail extends React.Component{
